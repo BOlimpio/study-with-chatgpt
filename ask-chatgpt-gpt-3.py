9d763e95cf8c1a9330a1987c7dc4ab2165deb4d6
BOlimpio <b.olimpio@outlook.com>
2023-03-09 00:37:38 -0300

    first version of study with chatgpt

---
+++ ask-chatgpt-gpt-3.py
@@ -0,0 +1,53 @@
+import requests
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file_name", help="Name of the file to save text document")
+args = parser.parse_args()
+
+if os.path.isfile(f"{args.file_name}.txt"):
+    file = open(f"{args.file_name}.txt", "a")
+else:
+    file = open(f"{args.file_name}.txt", "w")
+
+api_endpoint = "https://api.openai.com/v1/chat/completions"
+api_key = os.getenv("OPENAI_API_KEY")
+
+request_headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
+
+while True:
+    prompt = input("Enter a prompt (or 'done' to finish): ")
+    if prompt.lower() == "done":
+        break
+
+    request_data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": prompt}],
+        "max_tokens": 4000,
+        "temperature": 0.5
+    }
+
+    response = requests.post(api_endpoint, headers=request_headers, json=request_data)
+
+    if response.status_code == 200:
+        response_text = response.json()["choices"][0]["message"]["content"]
+        print(response_text)
+        satisfactory = input("\nWas the response satisfactory? (y/n): ")
+        while satisfactory.lower() not in ["y", "n"]:
+            satisfactory = input("\nPlease enter 'y' or 'n': ")
+        if satisfactory.lower() == "y":
+            file.write(f"{prompt} \n {response_text} \n" )
+            file.flush()
+            print ("\nResponse saved in document\n")
+        else:
+            continue
+    else:
+        response_error = response.json()["error"]["message"]
+        print(f"\n\nRequest failed with status code: {str(response.status_code)} and message: {response_error}")
+        break
+
+file.close()
